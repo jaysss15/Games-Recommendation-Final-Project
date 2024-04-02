d68a9fee792d1b5e97de8e6a92da5d60222d6b0b
jaysss <jasonlawrence088@gmail.com>
2024-04-02 22:56:33 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+import pandas
+from sklearn.tree import DecisionTreeClassifier
+
+data = pandas.read_csv("games.csv")
+x = data.drop(columns=["games"])
+y = data["games"]
+model = DecisionTreeClassifier()
+model.fit(x.values ,y)
+
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST","GET"])
+
+def home():
+    action = 0
+    strategy = 0
+    fantasy = 0
+    adventure = 0
+
+    if request.method == "POST":
+        Aaction = request.form["action"]
+        Sstrategy = request.form["strategy"]
+        Ffantasy = request.form["fantasy"]
+        Aadventure = request.form["adventure"]
+
+        action = Aaction
+        strategy = Sstrategy
+        fantasy = Ffantasy
+        adventure = Aadventure
+
+    predicted_value = model.predict([[action, strategy, fantasy, adventure]])
+
+    return render_template("index.html" ,firstname=predicted_value[0])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
