10d825a2cb3fe32a77469d0cd139dd80cf910cdf
jaysss <jasonlawrence088@gmail.com>
2024-04-02 23:03:35 +0800

    first commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 import pandas
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifie
 
 data = pandas.read_csv("games.csv")
 x = data.drop(columns=["games"])
